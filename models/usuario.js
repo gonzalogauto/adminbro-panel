0a94a46f212f82985a0b8fa41a7d6433ce008335
Gonzaa25 <gonzalo.gauto@gmail.com>
2020-10-23 22:04:52 -0300

    user title and avantarUrl

--- models/usuario.js
+++ models/usuario.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const UsuarioSchema = new Schema({
     email: { type: String, required: true },
+    title: { type: String, required: true },
+    avatarUrl: { type: String},
     encryptedPassword: { type: String, required: true },
     role: { type: String, enum: ['admin', 'restricted'], required: true },
 });
